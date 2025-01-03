c4a3c8eecf29d1cffa95777e4001c102c7ecebf4
Administrator <Administrator@i7E-31>
2025-01-03 13:02:43 +0900

    미안

---
+++ src/d0103/Variable.java
@@ -0,0 +1,14 @@
+package d0103;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 2;
+		// 배열
+		
+		int[] nums = new int[5];
+		
+		System.out.println(nums[0]);
+	}
+}
