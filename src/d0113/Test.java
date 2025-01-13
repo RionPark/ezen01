090fca27f042d5ad9b577866373b3db59a72c30d
Administrator <Administrator@i7E-31>
2025-01-13 12:46:16 +0900

    test

---
+++ src/d0113/Test.java
@@ -0,0 +1,16 @@
+package d0113;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 11;
+		if(num1<num2) {
+			System.out.println("Down");
+		}else if(num1>num2) {
+			System.out.println("Up");
+		}else {
+			System.out.println("맞췄다!");
+		}
+	}
+}
